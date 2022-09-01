9f54bd49d813fd560aafdd2b7917b3286901a940
Sean Patrick Hagen <sean.hagen@gmail.com>
2022-09-01 11:26:09 -0700

    Quick test

---
+++ cipher/cipher_test.go
@@ -0,0 +1,22 @@
+package cipher
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/seanhagen/cipherator/cipher/piglatin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCipher_GetEncoder(t *testing.T) {
+	var enc Encoder
+	var err error
+	var buf io.Writer
+
+	buf = bytes.NewBuffer(nil)
+	enc, err = New(EncoderTypePiglatin, buf)
+
+	assert.NoError(t, err)
+	assert.IsType(t, &piglatin.Encoder{}, enc)
+}
