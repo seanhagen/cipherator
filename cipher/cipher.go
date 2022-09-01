52ab2b10cb6053cfb59a2d378e4fa18cc967ebc2
Sean Patrick Hagen <sean.hagen@gmail.com>
2022-09-01 12:18:13 -0700

    Add ROT13 encoder type and update `New`

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -7,10 +7,11 @@ import (
 	"io"
 
 	"github.com/seanhagen/cipherator/cipher/piglatin"
+	"github.com/seanhagen/cipherator/cipher/rot13"
 )
 
 // EncoderType ...
-// ENUM(piglatin, reverse)
+// ENUM(piglatin, rot13)
 type EncoderType int32
 
 // Encoder
@@ -24,6 +25,8 @@ func New(t EncoderType, wr io.Writer) (Encoder, error) {
 	switch t {
 	case EncoderTypePiglatin:
 		return piglatin.New(wr)
+	case EncoderTypeRot13:
+		return rot13.New(wr)
 	}
 
 	return nil, fmt.Errorf("%v is an unknown encoder type", t.String())
