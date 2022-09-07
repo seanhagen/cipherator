88e11a32cf5ed36d6f8a259e954af464747533e4
Sean Patrick Hagen <sean.hagen@gmail.com>
2022-09-07 12:53:44 -0700

    Ensure the `New` constructor can handle ROT13

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -7,6 +7,7 @@ import (
 	"io"
 
 	"github.com/seanhagen/cipherator/cipher/piglatin"
+	"github.com/seanhagen/cipherator/cipher/rot13"
 )
 
 // EncoderType ...
@@ -36,6 +37,8 @@ func New(t EncoderType, wr io.Writer) (Encoder, error) {
 	switch t {
 	case EncoderTypePiglatin:
 		return piglatin.New(wr)
+	case EncoderTypeRot13:
+		return rot13.New(wr)
 	}
 
 	return nil, fmt.Errorf("%v is an unknown encoder type", t.String())
