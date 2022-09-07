16c7b6c788b4d2a11ac21ed4ffd56ebb7a14621e
Sean Patrick Hagen <sean.hagen@gmail.com>
2022-09-07 12:55:06 -0700

    Fill out the GoDoc comments

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -10,29 +10,31 @@ import (
 	"github.com/seanhagen/cipherator/cipher/rot13"
 )
 
-// EncoderType ...
+// EncoderType defines what types of encoders the system has available.
 // ENUM(piglatin, rot13)
 type EncoderType int32
 
-// Encoder ...
+// Encoder defines a type that can encode English text into encoded text
 type Encoder interface {
 	EncodeFromString(string) error
 	Encode(io.Reader) error
 }
 
-// Decoder ...
+// Decoder defines a type that decode text that was encoded by a specific cipher,
+// so a Pig Latin decoder can decode text into Enligsh text.
 type Decoder interface {
 	DecodeString(string) error
 	Decode(io.Reader) error
 }
 
-// Handler ...
+// Handler is a type that can handle both encoding and decoding for a cipher.
 type Handler interface {
 	Encoder
 	Decoder
 }
 
-// New ...
+// New returns a Handler for the specified EncoderType. It writes the output of either
+// the encoding or decoding operations to the io.Writer provided.
 func New(t EncoderType, wr io.Writer) (Handler, error) {
 	switch t {
 	case EncoderTypePiglatin:
